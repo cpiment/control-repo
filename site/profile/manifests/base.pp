9be46ef1e5b6e58a744343fb515e2525a7438add
cpiment <pimentel.carlos@gmail.com>
2016-09-07 12:34:14 +0200

    modificado profile base

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,18 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
 
+  class { '::ntp':
+    servers => [ '3.es.pool.ntp.org' ],
+  }
+
+  class { 'network':
+    config_file_notify => '',
+  }
+
+  network::mroute { 'enp0s3':
+    routes => {
+      '192.168.2.0/24' => '192.168.2.1',
+    }
+  }
+
 }
